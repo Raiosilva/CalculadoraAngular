ec8f07e85be0fda36fe84b67c4063b18153fb412
Raiosilva <raimundopi025@gmail.com>
2019-06-15 13:37:08 -0300

    setting initial and creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { ConversorModule } from './conversor/conversor.module';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ConversorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/conversor/conversor.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { ConversorComponent } from './components';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ConversorComponent
+  ],
+  exports: [
+    ConversorComponent
+  ]
+})
+export class ConversorModule { }
